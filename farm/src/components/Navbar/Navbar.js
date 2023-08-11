aa7510fa2177c2616d16a3da00ffb9060e2bfa2e
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-08-11 06:52:16 +0000

    src folders

--- farm/src/components/Navbar/Navbar.js
+++ farm/src/components/Navbar/Navbar.js
@@ -12,6 +12,13 @@ const Navbar = () => {
         <div className="mediaNavbarLogo">
           <h1>Excellent<i className='goldText'>Tech</i></h1>
         </div>
+        <div className="mediaNavbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">Why Us</a></p>
+          <p><a href="#prospects">Prospects</a></p>
+          <p><a href="#features">Features</a></p>
+          <p><a href="#blog">Blog</a></p>
+        </div>
       </div>
       <div className="mediaNavbar-sign">
         <span  className="signIn">
