692c077819ecd8d49e123c157ceea7d31fc5696c
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-08-11 08:00:06 +0000

    app files

--- farm/src/routes/Login.js
+++ farm/src/routes/Login.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import { LoginForm } from "../components/Login/LoginForm";
+
 function Login() {
   return (
     <>
-      <div>This Login page</div>
+      <LoginForm />
     </>
   );
 }
