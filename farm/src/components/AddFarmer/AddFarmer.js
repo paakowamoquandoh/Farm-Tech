d94e9f23067afe4e2a18d9716123fddde7f60e25
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-08-11 09:45:27 +0000

    farm commit

---
+++ farm/src/components/AddFarmer/AddFarmer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AddFarmer = () => (
+<div className="">
+
+   <div id="productListing" className="productListing hide">
+    <div className="addProduct">
+        <p className="productTitle">Application Successfull</p>
+        <button className="btn">add farmer</button>
+    </div>
+    <img src="./public/img/undraw_Reminders_re_gtyb.png" className="noProductImg" alt=""/>
+    <p className="noProductsText">No data</p>
+   </div>
+   <div className="backtohome">
+    <a href="/">Go Back to Homepage</a>
+   </div>
+   
+</div>
+);
+
+export default AddFarmer;
---
+++ farm/src/components/AgentForm/AgentForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const AgentFrom = () => (
+<div className="">
+<div className="logoContainer">
+    <img src={brandImg} className="logo" alt=""/>
+    </div>
+    <div className="sellerUser">
+    <p className="sellerHeading">Do you want to become an agent?</p>
+    <p className="sellDescription">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam voluptas ullam rem cupiditate quos eum similique totam ducimus, harum consequuntur alias eligendi! Possimus aliquam incidunt beatae corrupti accusantium cum iste. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum placeat explicabo quos quae sint suscipit itaque iste rem, doloremque magnam corrupti autem maxime incidunt perferendis ipsa, deserunt consectetur inventore. Alias?</p>
+    <span className="btn" id="applyBtn">apply below</span>
+   </div>
+
+   
+   <div class="applicationForm">
+    <input type="text" name="" id="businessName" placeholder="business name / company name / shop name"/>
+    <textarea cols="30" rows="10" id="addBusiness" placeholder="address"></textarea>
+    <textarea cols="30" rows="10" id="about" placeholder="about"></textarea>
+    <input type="text" id="number" placeholder="number"/>
+    <input type="checkbox" checked class="checkBox" id="termsAndConditions"/>
+    <label for="termsAndConditions">agree to our <a href="">terms and conditions</a></label><br/>
+    <input type="checkbox" checked class="checkBox" id="validInfo"/>
+    <label for="validInfo">all info is valid</label>
+    <button class="submitButton" id="applicationFormBtn">apply</button>
+   </div>
+   
+</div>
+);
+
+export default AgentFrom;
