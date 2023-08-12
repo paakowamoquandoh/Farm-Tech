50677d74b26d80b12f0aa2ac5a3cde15adffbee2
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-08-12 05:12:10 +0000

    admin commit

--- farm/src/components/AddFarmer/AddFarmer.js
+++ farm/src/components/AddFarmer/AddFarmer.js
@@ -1,23 +1,36 @@
 import React from "react";
 import noproductImg from "../../assets/icons/nopro.png"
+import "./addFarmer.css";
+import Modal from "../Modal/Modal";
+import Navbar from "../Navbar/Navbar";
+
 
 
 
 const AddFarmer = () => (
-<div className="">
+    
+<div>
+    <Navbar/>
+<div className="dashBox">
 
-   <div id="productListing" className="productListing">
-    <div className="addProduct">
-        <p className="productTitle">Application Successfull</p>
-        <button className="btn">add farmer</button>
-    </div>
-    <img src={noproductImg} className="noProductImg" alt=""/>
-    <p className="noProductsText">No data</p>
-   </div>
-   <div className="backtohome">
-    <a href="/">Go Back to Homepage</a>
-   </div>
-   
+
+<div id="productListing" className="productListing">
+
+ <div className="addProduct">
+     <p className="productTitle">Application Successfull</p>
+     <Modal />
+     {/* <button className="btn">add farmer</button> */}
+ </div>
+<div className="emptyBox">
+<img src={noproductImg} className="noProductImg" alt=""/>
+ <p className="noProductsText">No data</p>
+</div>
+</div>
+<div className="backtohome">
+ <a href="/">Go Back to Homepage</a>
+</div>
+
+</div>
 </div>
 );
 
---
+++ farm/src/components/Modal/Modal.js
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import "./modal.css"
+
+function Example() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  
+
+  return (
+    <>
+      <Button className='basicBtn' onClick={handleShow}>
+        Add Farmer
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+        
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+        
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default Example;
