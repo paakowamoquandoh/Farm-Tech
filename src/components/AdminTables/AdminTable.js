ffaea6a6695db2bdc14dcacb28ec3456f58de091
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-08-29 21:10:11 +0000

    farm tech commit

---
+++ src/components/AdminTables/AdminTable.js
@@ -0,0 +1,14 @@
+import {  } from "./hero.css";
+
+function Hero(props) {
+    return(
+        <>
+        <div className="text-white">
+           Tabular component
+        </div>
+        </>
+    )    
+}
+
+
+export default Hero;
---
+++ src/components/Reports/Reports.js
@@ -0,0 +1,14 @@
+import {  } from "./reports.css";
+
+function Reports() {
+    return(
+        <>
+        <div className="text-white">
+          im report
+        </div>
+        </>
+    )    
+}
+
+
+export default Reports;
