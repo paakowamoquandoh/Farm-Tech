1cf4cf9792a839a9a71c8140bc842d02aa075097
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-08-31 10:36:26 +0000

    Update HomeNav.js

--- src/components/HomeNav/HomeNav.js
+++ src/components/HomeNav/HomeNav.js
@@ -125,29 +125,12 @@ function HomeNav() {
             )}
             {toggleMenu && (
               <div className="mediaNavbar-menu_container scale-up-center">
-                <div className="search-wrapper">
-                  <input
-                    className="search-input"
-                    type="text"
-                    placeholder="Search"
-                  />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="feather feather-search"
-                    viewBox="0 0 24 24"
-                  >
-                    <defs></defs>
-                    <circle cx="11" cy="11" r="8"></circle>
-                    <path d="M21 21l-4.35-4.35"></path>
-                  </svg>
-                </div>
+               <ul>
+                <li><a href="/admin">Home</a></li>
+                <li><a href="/admin/admintable">Tables</a></li>
+                <li><a href="/admin/reports">Reports</a></li>
+                <li><a href="/admin/settting">Settings</a></li>
+               </ul>
               </div>
             )}
           </div>
