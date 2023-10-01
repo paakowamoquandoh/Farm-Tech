7c97147c87455dbb9c6e27107acb02513dd1eab6
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-10-01 21:47:39 +0000

    app update

--- src/components/Login/AdminLogin.js
+++ src/components/Login/AdminLogin.js
@@ -1,9 +1,8 @@
 import React, { useRef, useState } from "react"
-import { Form, Button, Alert } from "react-bootstrap"
+import { Form, Alert } from "react-bootstrap"
 import { useAuth } from "../../contexts/AuthContext"
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import "./login.css"
-import brandImg from "../../assets/icons/brand.png"
 
 export default function Login() {
   const emailRef = useRef()
@@ -31,30 +30,27 @@ export default function Login() {
   return (
     <>
       <div className="adminformBody">
-      <div className="logoContainer">
-        <img src={brandImg} className="logo" alt=""/>
-       </div>
         <div>
-          <h2 className="text-center text-white mb-4">Log In</h2>
           {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
+          <Form className="form" onSubmit={handleSubmit}>
+          <h2 className="text-center text-black mb-4">Farm Tech</h2>
             <Form.Group id="email">
               <Form.Control placeholder="enter email" type="email" ref={emailRef} required />
             </Form.Group>
             <Form.Group id="password">
               <Form.Control placeholder="password" type="password" ref={passwordRef} required />
             </Form.Group>
-            <Button disabled={loading} className="w-100 adminloginBtn" type="submit">
+            <button disabled={loading} className="adminloginBtn" type="submit">
               Log In
-            </Button>
+            </button>
           </Form>
-          <div className="w-100 text-center text-white mt-3">
+          {/* <div className="w-100 text-center text-white mt-3">
             <Link to="/forgot-password">Forgot Password?</Link>
-          </div>
+          </div> */}
         </div>
-        <div className="loginLink text-white">
+        {/* <div className="loginLink text-white">
          <Link to="/contactus">Contact us</Link> for a link to <Link to="/signup">Sign Up.</Link>
-      </div>
+      </div> */}
       </div>
       
     </>
